e328a945dd89ea11537c47c81f9cbf63c7a2833b
christianangeles <christian.angeles@smoothstack.com>
2021-05-25 19:23:36 -0700

    change repo file structure

--- week6-day2/user-data.sh
+++ week6-day2/user-data.sh
@@ -3,4 +3,4 @@ yum update -y
 yum install git -y
 cd /opt
 git clone https://github.com/christianangeles/ss-cloud-apr2021-aws.git
-python3 /opt/ss-cloud-apr2021-aws/ec2-userdata-hello.py
+python3 /opt/ss-cloud-apr2021-aws/week6-day2/ec2-userdata-hello.py
