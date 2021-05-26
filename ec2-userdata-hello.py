005b7b333579376ef780773f6147cde774e71509
christianangeles <christian.angeles@smoothstack.com>
2021-05-25 19:03:44 -0700

    update python and userdata

--- ec2-userdata-hello.py
+++ ec2-userdata-hello.py
@@ -9,14 +9,14 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b'Hello world')
 
-print("hello world starting at port 8000")
-httpd = socketserver.TCPServer(('', 80), Handler)
+print("hello world starting at port 8080")
+httpd = socketserver.TCPServer(('', 8080), Handler)
 httpd.serve_forever()
 
 
 # python3 server.py
 # 127.0.0.1 - - [11/Apr/2017 11:36:49] "GET / HTTP/1.1" 200 -
-# http :8000
+# http :8080
 '''
 HTTP/1.0 200 OK
 Date: Tue, 11 Apr 2017 15:36:49 GMT
--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum update -y
 yum install git -y
+cd /opt
 git clone https://github.com/christianangeles/ss-cloud-apr2021-aws.git
-python3 ss-cloud-apr2021-aws/ec2-userdata-hello.py
+python3 /opt/ss-cloud-apr2021-aws/ec2-userdata-hello.py
